89482fd1ded3bec6f5e9e6c138fd8cda9e03613d
Joan Gladbach <jjgr29@umsystem.edu>
2024-11-25 11:53:03 -0600

    Add project files.

---
+++ CS201L-Lab2b/CS201L-Lab2b.cpp
@@ -0,0 +1,14 @@
+// NAME:
+// LAB SECTION:
+// LECTURE SECTION:
+// CS201L-Lab2b.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to My Conversion Program\n";
+
+}
